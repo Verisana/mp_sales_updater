00b847ae90f3fb81d1046e2df667293b14e73ba5
AssaNix <1902Grand@gmail.com>
2020-06-23 20:23:37 +0500

    Added app mp_main_db_models

---
+++ mp_main_db_models/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class MarketPlaces(models.Model):
+    pass
+    #question_text = models.CharField(max_length=200)
+    #pub_date = models.DateTimeField('date published')
+
+
+class Items(models.Model):
+    pass
+
+
+class ItemUpdates(models.Model):
+    pass
+
