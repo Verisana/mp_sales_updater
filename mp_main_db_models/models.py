70f415b105fcd3aa788061bf581e067f3d816ad6
AssaNix <1902Grand@gmail.com>
2020-06-23 22:49:51 +0500

    Added main model schemes without type info

--- mp_main_db_models/models.py
+++ mp_main_db_models/models.py
@@ -2,15 +2,51 @@ from django.db import models
 
 
 class MarketPlaces(models.Model):
-    pass
-    #question_text = models.CharField(max_length=200)
-    #pub_date = models.DateTimeField('date published')
+    name = None
+    created_at = None
+    modified_at = None
+    working_scheme = None
+
+
+class ItemCategories(models.Model):
+    name = None
+    mp_source = None
+    created_at = None
+    modified_at = None
+
+
+class ItemSellers(models.Model):
+    name = None
+    mp_source = None
+    created_at = None
+    modified_at = None
+    categories = None
 
 
 class Items(models.Model):
-    pass
+    name = None
+    mp_source = None
+    categories = None
+    seller = None
+    sku = None
+    brand = None
+
+    created_at = None
+    modified_at = None
+
+    last_update = None
 
 
 class ItemUpdates(models.Model):
-    pass
+    item = None
+    created_at = None
 
+    rating = None
+    coments_num = None
+    main_image = None
+    is_new = None
+    is_bestseller = None
+
+    full_price = None
+    discount = None
+    available_qty = None
