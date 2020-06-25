e81240c739b1c1d20151c1f98b74b48b44b41087
Verisana <account@0112.pro>
2020-06-25 22:59:18 +0500

    Working on scraper. Added base class and started defining it

--- core/mp_scrapers/wildberries.py
+++ core/mp_scrapers/wildberries.py
@@ -1,5 +1,15 @@
+from .base import ScraperConfigs, BaseScraper
 
 
-class WildberriesScraper:
-    def __init__(self):
-        pass
+WILDBERRIES_CONFIG = ScraperConfigs(
+    categories_url='',
+    item_url='',
+    revision_url='',
+)
+
+
+class WildberriesScraper(BaseScraper):
+    def __init__(self, use_proxy: bool=True):
+        super().__init__((), ())
+        self.urls = []
+        self.proxies = []
