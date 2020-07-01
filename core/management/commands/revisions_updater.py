9ea0be457903f676c5af502881f16bbf91662c42
Verisana <account@0112.pro>
2020-07-01 22:24:39 +0500

    One command script start, each class in separate file, done writing item increment_execute. It works very slow.

--- core/management/commands/revisions_updater.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = 'Start listening for revision updates'
-
-    def handle(self, *args, **options):
-        pass
---
+++ core/mp_scrapers/wildberries/wildberries_categories.py
@@ -0,0 +1,159 @@
+import re
+import pickle
+from typing import List
+
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+from mptt.querysets import TreeQuerySet
+
+from core.mp_scrapers.configs import WILDBERRIES_CONFIG
+from core.utils.connector import Connector
+from core.types import RequestBody
+from core.utils.trees import Node
+from core.models import Marketplace, MarketplaceScheme, ItemCategory
+
+
+class WildberriesCategoryScraper:
+    def __init__(self):
+        self.config = WILDBERRIES_CONFIG
+        self.connector = Connector(use_proxy=self.config.use_proxy)
+        self.all_categories_headers = {
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/83.0.4103.116 Safari/537.36',
+            'x-requested-with': 'XMLHttpRequest',
+        }
+        self.base_catalog_pattern = 'https:\/\/www.wildberries.ru\/catalog\/{}'
+        self.mp_source = self._get_mp_wb()
+
+    def update_from_mp(self) -> None:
+        bs, is_captcha = self.connector.get_page(RequestBody(self.config.base_categories_url, 'get',
+                                                             headers=self.all_categories_headers))
+        parsed_nodes = self._parse_bs_response(bs)
+        with open('parsed_nodes.p', 'wb') as f:
+            pickle.dump(parsed_nodes, f)
+
+        self._check_db_consistency()
+
+    def update_from_file(self, load_file: str):
+        parsed_nodes = pickle.load(open(load_file, 'rb'))
+        self._save_all_results_in_db(parsed_nodes)
+        self._check_db_consistency()
+
+    def _get_mp_wb(self) -> Marketplace:
+        scheme_qs = MarketplaceScheme.objects.get_or_create(name='FBM')[0]
+        mp_wildberries, is_created = Marketplace.objects.get_or_create(name='Wildberries')
+        if is_created:
+            mp_wildberries.working_schemes.add(scheme_qs)
+            mp_wildberries.save()
+        return mp_wildberries.id
+
+    def _parse_bs_response(self, bs: BeautifulSoup) -> List[Node]:
+        root_node_tags = bs.find('ul', class_='topmenus').findAll(self._check_root_matching)
+
+        root_nodes = []
+        for tag in root_node_tags:
+            name, url = tag.text, tag.find('a')['href']
+            new_node = Node(name, url, parent=None)
+            category, _ = ItemCategory.objects.get_or_create(name=new_node.name, mp_category_url=new_node.mp_url,
+                                                             mp_source=self.mp_source, level=0)
+            new_node.db_id = category.id
+            root_nodes.append(new_node)
+
+        return self._parse_all_descendants(root_nodes)
+
+    def _check_root_matching(self, tag: Tag) -> bool:
+        """
+        :param tag: Tag - tag to check
+        :return:
+        1. Choose only 'li'
+        2. We need only two tags to exclude premium/travels etc. tags
+        3. Certain class names to include
+        4. Exclude not root links. For example: .../catalog/detyam/shkola
+        """
+        return tag.name == 'li' and \
+               len(tag['class']) == 2 and \
+               ' '.join(tag['class']) == 'topmenus-item j-parallax-back-layer-item' and \
+               re.fullmatch(self.base_catalog_pattern.format('[^\/\?]+'), tag.find('a')['href'])
+
+    def _parse_all_descendants(self, nodes: List[Node], level: int = 0, root: Node = None) -> List[Node]:
+        for i, node in enumerate(nodes):
+            # Для отладки
+            if level in [0, 1]:
+                import time
+                import datetime
+                hour = datetime.datetime.fromtimestamp(time.time()).hour
+                minute = datetime.datetime.fromtimestamp(time.time()).minute
+                second = datetime.datetime.fromtimestamp(time.time()).second
+                if level == 0:
+                    print(f'{i+1}/{len(nodes)} - {node.name}, time: {hour:00.0f}:{minute:00.0f}:{second:00.0f}, '
+                          f'level {level}')
+                elif level == 1:
+                    print(f'\t{i+1}/{len(nodes)} - {node.name}, time: {hour:00.0f}:{minute:00.0f}:{second:00.0f}, '
+                          f'level {level}')
+            # Прям до сюда
+            descendants_bs, is_captcha = self.connector.get_page(RequestBody(node.mp_url, 'get'))
+
+            if level == 0:
+                try:
+                    all_items = descendants_bs.find('ul', class_='maincatalog-list-2').findAll('li')
+                except AttributeError:
+                    all_items = descendants_bs.find('ul', class_='maincatalog-list-1').findAll('li')
+
+                for item in all_items:
+                    url = self.config.base_url + item.find('a')['href']
+                    node.descendants.append(self._get_node(item.text, url, node, level))
+            else:
+                try:
+                    all_items = descendants_bs.find('div', class_='catalog-sidebar').findAll('li')
+                except AttributeError as e:
+                    continue
+
+                is_descendants_started = False
+                for item in all_items:
+                    try:
+                        item_class = item['class']
+                    except KeyError:
+                        item_class = []
+                    if 'hasnochild' in item_class:
+                        is_descendants_started = True
+                        continue
+                    if is_descendants_started:
+                        url = self.config.base_url + item.find('a')['href']
+                        node.descendants.append(self._get_node(item.text, url, node, level))
+            self._parse_all_descendants(node.descendants, level+1)
+        return nodes
+
+    def _get_node(self, name: str, url: str, parent: Node, level: int) -> Node:
+        new_node = Node(name, url, parent=parent)
+        category_id = self._save_node_in_db(new_node, parent, level)
+        new_node.db_id = category_id
+        return new_node
+
+    def _save_node_in_db(self, node: Node, parent_node: Node, level: int) -> int:
+        parent = ItemCategory.objects.get(name=parent_node.name, mp_category_url=parent_node.mp_url,
+                                          mp_source=self.mp_source, level=level)
+        category, _ = ItemCategory.objects.get_or_create(name=node.name, mp_category_url=node.mp_url,
+                                                         mp_source=self.mp_source, parent=parent)
+        return category.id
+
+    def _check_db_consistency(self) -> None:
+        return None
+
+    def _save_all_results_in_db(self, parsed_nodes: List[Node], parent: TreeQuerySet = None) -> None:
+        for parsed_node in parsed_nodes:
+            if parent is not None:
+                category, is_created = ItemCategory.objects.get_or_create(name=parsed_node.name,
+                                                                          mp_source=self.mp_source,
+                                                                          parent=parent)
+            else:
+                # If you don't specify level, you get multiple objects because
+                category, is_created = ItemCategory.objects.get_or_create(name=parsed_node.name,
+                                                                          mp_source=self.mp_source,
+                                                                          level=0)
+            category.mp_category_url = parsed_node.mp_url
+            category.save()
+            parsed_node.db_id = category.id
+            self._save_all_results_in_db(parsed_node.descendants, parent=category)
