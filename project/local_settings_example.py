f22b8c96f2fc78059cf54c47926b3559e8cf3105
Verisana <account@0112.pro>
2020-07-27 21:43:11 +0500

    Changed local settings and requirenments

--- project/local_settings_example.py
+++ project/local_settings_example.py
@@ -8,20 +8,60 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['', '']
 
+INTERNAL_IPS = ['127.0.0.1']
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
-EMAIL_HOST = ''
-EMAIL_PORT = ''
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+JSON_LOG_FILE = 'logs/main.json.log'
+DJANGO_DEBUG_LOG_FILE = 'logs/django.json.log'
+TELEGRAM_TOKEN = ''
+TELEGRAM_LOG_CHAT_ID = ''
 
-CELERY_BROKER_URL = 'amqp://localhost'
-
-ADMINS = []
-
-INTERNAL_IPS = ['127.0.0.1']
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s: %(levelname)s %(module)s (%(process)d): %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'FullJsonFormatter': {
+            '()': 'core.utils.logging_helpers.FullJsonFormatter'
+        },
+    },
+    'handlers': {
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': JSON_LOG_FILE,
+            'level': 'INFO',
+            'formatter': 'FullJsonFormatter'
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'verbose'
+        },
+        'telegram_error': {
+            'class': 'core.utils.logging_helpers.TelegramHandler',
+            'token': TELEGRAM_TOKEN,
+            'chat_id': TELEGRAM_LOG_CHAT_ID,
+            'level': 'WARNING',
+            'formatter': 'FullJsonFormatter',
+        },
+    },
+    'loggers': {
+        'main': {
+            'handlers': ['console', 'file', 'telegram_error'],
+            'level': 'DEBUG',
+        },
+    }
+}
