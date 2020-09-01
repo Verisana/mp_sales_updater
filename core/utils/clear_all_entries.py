b4c3a4bf00d66508313998cfe80f0abf7f469d60
Verisana <1902Grand@gmail.com>
2020-09-01 15:25:35 +0500

    Rearranged script delete_entries working order

--- core/utils/clear_all_entries.py
+++ core/utils/clear_all_entries.py
@@ -2,9 +2,9 @@ from core.models import Item, ItemRevision, Colour, Brand, Image, Seller
 
 
 def delete_all_entries():
-    print(Item.objects.all().delete())
     print(ItemRevision.objects.all().delete())
     print(Colour.objects.all().delete())
-    print(Image.objects.all().delete())
-    print(Brand.objects.all().delete())
     print(Seller.objects.all().delete())
+    print(Brand.objects.all().delete())
+    print(Image.objects.all().delete())
+    print(Item.objects.all().delete())
